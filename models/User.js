923bd438804fc016c8d6e11cc50cbf7af677878b
goeunP <0young384@gmail.com>
2023-04-06 22:33:33 +0900

    set register data encoded

--- models/User.js
+++ models/User.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -19,6 +22,22 @@ const userSchema = mongoose.Schema({
   toeknExp: {
     type: Number,
   },
+});
+
+userSchema.pre("save", function (next) {
+  var user = this;
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
 });
 
 const User = mongoose.model("User", userSchema);
